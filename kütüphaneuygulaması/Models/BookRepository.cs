052a08602a67294849abe1d813bf010ba6ce1760
yuncubugrahan <yuncubugrahan@gmail.com>
2026-05-17 14:54:41 +0300

    Hafta 4 - BookRepository eklendi

---
+++ kütüphaneuygulaması/Models/BookRepository.cs
@@ -0,0 +1,45 @@
+﻿namespace kütüphaneuygulaması.Models
+{
+    public class BookRepository
+    {
+        private static List<Book> _books = new List<Book>
+        {
+            private static List<Book> _books = new List<Book>();
+    };
+
+        public List<Book> GetAll()
+        {
+            return _books;
+        }
+
+        public void Add(Book newBook)
+        {
+            newBook.Id = _books.Count > 0 ? _books.Max(b => b.Id) + 1 : 1;
+            _books.Add(newBook);
+        }
+
+        public void Remove(int id)
+        {
+            var book = _books.FirstOrDefault(b => b.Id == id);
+            if (book != null)
+                _books.Remove(book);
+        }
+
+        public Book? GetById(int id)
+        {
+            return _books.FirstOrDefault(b => b.Id == id);
+        }
+
+        public void Update(Book updatedBook)
+        {
+            var book = _books.FirstOrDefault(b => b.Id == updatedBook.Id);
+            if (book != null)
+            {
+                book.Title = updatedBook.Title;
+                book.Author = updatedBook.Author;
+                book.Price = updatedBook.Price;
+                book.Stock = updatedBook.Stock;
+            }
+        }
+    }
+}
